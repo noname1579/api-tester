e7b14c0123f00aacfa0118715c51c999efac30b3
noname1579 <ilyalashenko115@gmail.com>
2025-07-23 11:50:17 +0300

    тест gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/story-stream/',
+  base: '/api-tester/',
   plugins: [react(), tailwindcss()]
 })
